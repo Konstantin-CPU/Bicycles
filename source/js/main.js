96f1c93a13374ac410445b30584e47c48dcdcd4a
Константин <konstantingoncharuk50@gmail.com>
2021-11-07 19:40:41 +0300

    Правки перед тестом

--- source/js/main.js
+++ source/js/main.js
@@ -1,15 +1,19 @@
 'use strict';
-const pageNavList = document.querySelector(".page-header__navigation");
-const navToggle = document.querySelector(".page-header__toggle");
 
-pageNavList.classList.remove("page-header__navigation--nojs");
+window.onload = function() {
+  const pageNavList = document.querySelector(".page-header__navigation");
+  const navToggle = document.querySelector(".page-header__toggle");
+  const pageBody = document.querySelector(".page__body");
 
-navToggle.addEventListener("click", function() {
-  if (pageNavList.classList.contains("page-header__navigation--closed")) {
-    pageNavList.classList.remove("page-header__navigation--closed");
-    pageNavList.classList.add("page-header__navigation--opened");
-  } else {
-    pageNavList.classList.remove("page-header__navigation--opened");
-    pageNavList.classList.add("page-header__navigation--closed");
-  }
-})
+  navToggle.addEventListener("click", function() {
+    if (pageNavList.classList.contains("page-header__navigation--closed")) {
+      pageNavList.classList.remove("page-header__navigation--closed");
+      pageNavList.classList.add("page-header__navigation--opened");
+      pageBody.classList.add("page__body--scroll-disabled");
+    } else {
+      pageNavList.classList.remove("page-header__navigation--opened");
+      pageNavList.classList.add("page-header__navigation--closed");
+      pageBody.classList.remove("page__body--scroll-disabled");
+    }
+  })
+}
