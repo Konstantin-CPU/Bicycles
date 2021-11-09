3b8f467fb8c6f90b299299e4db827925cabc5300
Константин <konstantingoncharuk50@gmail.com>
2021-11-09 20:22:03 +0300

    Правки по тестированию

--- build/js/main.js
+++ build/js/main.js
@@ -1,19 +1,32 @@
 'use strict';
 
-window.onload = function() {
-  const pageNavList = document.querySelector(".page-header__navigation");
-  const navToggle = document.querySelector(".page-header__toggle");
-  const pageBody = document.querySelector(".page__body");
+const pageNavList = document.querySelector(".page-header__navigation");
+const navToggle = document.querySelector(".page-header__toggle");
+const pageBody = document.querySelector(".page__body");
+const telInput = document.querySelector(".info-form__input--tel");
 
-  navToggle.addEventListener("click", function() {
-    if (pageNavList.classList.contains("page-header__navigation--closed")) {
-      pageNavList.classList.remove("page-header__navigation--closed");
-      pageNavList.classList.add("page-header__navigation--opened");
-      pageBody.classList.add("page__body--scroll-disabled");
-    } else {
-      pageNavList.classList.remove("page-header__navigation--opened");
-      pageNavList.classList.add("page-header__navigation--closed");
-      pageBody.classList.remove("page__body--scroll-disabled");
-    }
-  })
-}
+let reg = /[A-Za-zА-яа-яЁё]/g
+
+telInput.addEventListener("input", function() {
+  this.value = this.value.replace(reg, "");
+});
+
+const navLink = document.querySelectorAll(".page-header__navigation-link").forEach(element => {
+  element.addEventListener("click", function() {
+    pageNavList.classList.remove("page-header__navigation--opened");
+    pageNavList.classList.add("page-header__navigation--closed");
+    pageBody.classList.remove("page__body--scroll-disabled");
+  });
+}) ;
+
+navToggle.addEventListener("click", function() {
+  if (pageNavList.classList.contains("page-header__navigation--closed")) {
+    pageNavList.classList.remove("page-header__navigation--closed");
+    pageNavList.classList.add("page-header__navigation--opened");
+    pageBody.classList.add("page__body--scroll-disabled");
+  } else {
+    pageNavList.classList.remove("page-header__navigation--opened");
+    pageNavList.classList.add("page-header__navigation--closed");
+    pageBody.classList.remove("page__body--scroll-disabled");
+  }
+})
